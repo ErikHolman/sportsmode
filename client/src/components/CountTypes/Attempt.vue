4e2c10ccefab3dd03145fc4825a5ad1083a045bb
Erik Holman <erik@holman.dev>
2024-09-19 10:04:23 -0700

    Adding new discrete components

---
+++ client/src/components/CountTypes/Attempt.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Counter from '../Counter.vue';
+import { ref } from 'vue';
+
+const props = defineProps({
+    currentCount: Number,
+});
+
+const emit = defineEmits(['updateAttempt']);
+
+const attemptType = { name: 'attempt', value: 1 };
+
+function sendUpdate(event) {
+    emit('updateAttempt', event);
+}
+</script>
+
+<template>
+    <Counter
+        :count="props.currentCount"
+        :kind="attemptType.name"
+        :value="attemptType.value"
+        @updateCount="sendUpdate"
+    />
+</template>
+
+<style></style>
---
+++ client/src/components/CountTypes/Points.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Counter from '../Counter.vue';
+
+const props = defineProps({
+    currentCount: Number,
+    kind: String,
+    value: Number,
+});
+
+const emit = defineEmits(['updatePoints']);
+
+function sendUpdate(event) {
+    emit('updatePoints', { kind: props.kind, value: event });
+}
+</script>
+
+<template>
+    <Counter
+        :count="currentCount"
+        :kind="props.kind"
+        :value="props.value"
+        @updateCount="sendUpdate"
+    />
+</template>
+
+<style></style>
